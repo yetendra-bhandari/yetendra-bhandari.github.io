180ac8eabd05f6e223e33a0ee88100a28139b58e
Yetendra Bhandari <yetendrabhandari@gmail.com>
2021-02-26 23:56:17 +0530

    completed first section of portfolio

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <main class="flex flex-col">
+    <h1 class="mt-24 text-8xl font-bold text-center">Hello, I'm Yetendra</h1>
+    <h2 class="mt-10 text-5xl text-center leading-tight">
+      I develop frontend and backend web-applications using
+      <a
+        class="hover:underline focus:underline"
+        href="https://vuejs.org/"
+        target="_blank"
+        title="Vue"
+        >Vue</a
+      >
+      and
+      <a
+        class="hover:underline focus:underline"
+        href="https://reactjs.org/"
+        target="_blank"
+        title="React"
+        >React
+      </a>
+    </h2>
+    <section class="mt-16 flex flex-wrap justify-center gap-4">
+      <a
+        href="/Yetendra-Bhandari-Resume.pdf"
+        target="_blank"
+        class="p-4 border-2 rounded border-color-2 text-2xl text-text-2 hover:bg-color-2 hover:text-text-3 focus:bg-color-2 focus:text-text-3"
+        title="My Resume"
+        >Look At My Resume
+      </a>
+      <a
+        href="mailto:yetendrabhandari@gmail.com"
+        target="_blank"
+        class="p-4 border-2 rounded border-color-2 text-2xl text-text-2 hover:bg-color-2 hover:text-text-3 focus:bg-color-2 focus:text-text-3"
+        title="Talk To Me"
+        >Talk To Me
+      </a>
+    </section>
+    <hr class="mt-12 border-color-3" />
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["var(--font-body)", "sans-serif"]
+      },
+      colors: {
+        "color-1": "var(--color-1)",
+        "color-2": "var(--color-2)",
+        "color-3": "var(--color-3)",
+        "text-1": "var(--text-1)",
+        "text-2": "var(--text-2)",
+        "text-3": "var(--text-3)",
+        "text-4": "var(--text-4)"
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
